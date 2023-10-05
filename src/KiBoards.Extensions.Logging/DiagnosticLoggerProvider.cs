b31a33c31f871047350ca0d5681b59cb50886a72
Matt <matt.janda.kingston@gmail.com>
2023-10-05 12:05:19 +0100

    Add test output helper to access test case details and logger configuration extensions

--- src/KiBoards.Extensions.Logging/DiagnosticLoggerProvider.cs
+++ src/KiBoards.Extensions.Logging/DiagnosticLoggerProvider.cs
@@ -6,15 +6,17 @@ namespace KiBoards.Extensions.Logging
     internal class DiagnosticLoggerProvider : ILoggerProvider
     {
         private readonly IMessageSink _messageSink;
+        private readonly ITestOutputHelper _outputHelper;
 
-        public DiagnosticLoggerProvider(IMessageSink messageSink)
+        public DiagnosticLoggerProvider(IMessageSink messageSink, ITestOutputHelper outputHelper)
         {
             _messageSink = messageSink;
+            _outputHelper = outputHelper;
         }
 
         public ILogger CreateLogger(string categoryName)
         {
-            return new DiagnosticLogger(_messageSink);
+            return new DiagnosticLogger(_messageSink, _outputHelper);
         }
 
         public void Dispose()
---
+++ src/KiBoards.Extensions.Logging/TestOutputHelperExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using Xunit.Abstractions;
+
+namespace KiBoards.Extensions.Logging
+{
+    internal static class TestOutputHelperExtensions
+    {
+        public static ITestCase GetTestCase(this ITestOutputHelper output)
+            => (output.GetType().GetField("test", BindingFlags.Instance | BindingFlags.NonPublic).GetValue(output) as ITest).TestCase;
+
+        public static ITest GetTest(this ITestOutputHelper output)
+            => (output.GetType().GetField("test", BindingFlags.Instance | BindingFlags.NonPublic).GetValue(output) as ITest);
+
+    }
+}
