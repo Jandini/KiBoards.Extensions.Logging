4bee1ae7a0ddc56757cee84e67672a40ea468f70
Matt <matt.janda.kingston@gmail.com>
2023-10-24 19:55:48 +0100

    Add KiBoards startup class to obtain run id

---
+++ src/KiBoards.Extensions.Logging/KiBoardsTestRun.cs
@@ -0,0 +1,17 @@
+﻿[assembly: KiboardsTestStartup("KiBoards.Extensions.Logging.KiBoardsTestRun")]
+
+namespace KiBoards.Extensions.Logging
+{
+    public class KiBoardsTestRun
+    {
+        public static KiBoardsTestRun Instance { get; private set; }
+        
+        public string Id { get; set; }
+
+        public KiBoardsTestRun(string id) 
+        { 
+            Id = id;
+            Instance = this;
+        }
+    }
+}
