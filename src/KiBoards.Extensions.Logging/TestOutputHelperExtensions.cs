bededeb81de8d6e6d65ad7f02485cb839d81becc
Matt <matt.janda.kingston@gmail.com>
2023-10-24 14:18:12 +0100

    Add test ouput helper extension for logger begin scope

--- src/KiBoards.Extensions.Logging/TestOutputHelperExtensions.cs
+++ src/KiBoards.Extensions.Logging/TestOutputHelperExtensions.cs
@@ -1,4 +1,5 @@
-﻿using System.Reflection;
+﻿using Microsoft.Extensions.Logging;
+using System.Reflection;
 using Xunit.Abstractions;
 
 namespace KiBoards.Extensions.Logging
@@ -11,5 +12,15 @@ namespace KiBoards.Extensions.Logging
         public static ITest GetTest(this ITestOutputHelper output)
             => (output.GetType().GetField("test", BindingFlags.Instance | BindingFlags.NonPublic).GetValue(output) as ITest);
 
+
+        public static IDisposable BeginScope<T>(this ILogger<T> logger, ITestOutputHelper output)
+        {
+            var testCase = GetTestCase(output);
+
+            return logger.BeginScope(new Dictionary<string, object>()
+            {
+                ["UniqueId"] = testCase.UniqueID,
+            });
+        }
     }
 }
