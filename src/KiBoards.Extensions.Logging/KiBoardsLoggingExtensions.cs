9c77cb06df82440f6a04ac0bddbf035388b04aab
Matt Janda <matt.janda.kingston@gmail.com>
2023-10-25 18:16:42 +0100

    Remove test scope and use property enrichment (#4)

--- src/KiBoards.Extensions.Logging/KiBoardsLoggingExtensions.cs
+++ src/KiBoards.Extensions.Logging/KiBoardsLoggingExtensions.cs
@@ -8,20 +8,11 @@ namespace KiBoards.Extensions.Logging
 {
     public static class KiBoardsLoggingExtensions
     {
-        public static IDisposable TestScope<T>(this ILogger<T> logger, ITestOutputHelper output)
+     
+        public static IServiceCollection AddKiBoardsLogging(this IServiceCollection services, ITestOutputHelper testOutputHelper, Func<ILoggingBuilder, ILoggingBuilder> configure)
         {
-            var testCase = output.GetTestCase();
+            var testCase = testOutputHelper.GetTestCase();
 
-            return logger.BeginScope(new Dictionary<string, object>()
-            {
-                ["TestCaseId"] = (KiBoardsTestRun.Instance.Id + testCase.UniqueID).ComputeMD5(),
-                ["TestRunId"] = KiBoardsTestRun.Instance.Id,
-                ["TestCaseUniqueId"] = testCase.UniqueID
-            });
-        }
-
-        public static IServiceCollection AddKiBoardsLogging(this IServiceCollection services, Func<ILoggingBuilder, ILoggingBuilder> configure)
-        {
             var elasticOptions = new ElasticsearchSinkOptions(new Uri(Environment.GetEnvironmentVariable("KIB_ELASTICSEARCH_HOST") ?? "http://localhost:9200"))
             {
                 IndexFormat = $"kiboards-testlogs-{DateTime.UtcNow:yyyy-MM}",
@@ -30,11 +21,13 @@ namespace KiBoards.Extensions.Logging
 
             return services.AddLogging(builder => configure(builder).AddSerilog(new LoggerConfiguration()
                 .WriteTo.Elasticsearch(elasticOptions)
+                .Enrich.WithProperty("TestCaseId", (KiBoardsTestRun.Instance.Id + testCase.UniqueID).ComputeMD5())
+                .Enrich.WithProperty("TestRunId", KiBoardsTestRun.Instance.Id)
+                .Enrich.WithProperty("TestCaseUniqueId", testCase.UniqueID)
                 .CreateLogger(), true));
         }
 
-        public static IServiceCollection AddKiBoardsLogging(this IServiceCollection services) => AddKiBoardsLogging(services, (config) => config);
-        public static IServiceCollection AddKiBoardsLogging(this IServiceCollection services, LogLevel minimumLogLevel) => AddKiBoardsLogging(services, (config) => config.SetMinimumLevel(minimumLogLevel));
-       
+        public static IServiceCollection AddKiBoardsLogging(this IServiceCollection services, ITestOutputHelper testOutputHelper) => AddKiBoardsLogging(services, testOutputHelper, (config) => config);
+        public static IServiceCollection AddKiBoardsLogging(this IServiceCollection services, ITestOutputHelper testOutputHelper, LogLevel minimumLogLevel) => AddKiBoardsLogging(services, testOutputHelper, (config) => config.SetMinimumLevel(minimumLogLevel));       
     }
 }
